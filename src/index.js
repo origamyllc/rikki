969fc03cbaeae5855d499ae284cc59a74708e320
Prashun Javeri <origamyllc@gmail.com>
2016-10-11 19:52:07 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,21 +9,16 @@ const seed_path = process.env.MONGO_SEED_PATH || "";
 let seeds ;
 
 if(seed_path){
-   seeds =  require(seed_path).seeds;
-
+    seeds =  require(seed_path).seeds;
     Object.keys(seeds)
         .forEach((collection) => {
-
             let  collectionArray = seeds[collection];
             collectionArray.forEach((obj) => {
                 engine.insert( collection ,  obj);
             });
-
             engine.show(collection).then((result) => {
                 console.log(result)
             });
-
         });
-
 }
 
